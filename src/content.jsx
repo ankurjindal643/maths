2beee97ca68ba19a9ae600d727be9b6e561f7412
hce <vmalikv87@gmail.com>
2021-09-07 23:53:00 +0530

    up

--- src/content.jsx
+++ src/content.jsx
@@ -4,12 +4,12 @@ let Content = () => {
   return (
     <>
       <div className="introduction">
-        <div className="quote">
-          <p className="small1">LIFE IS A MATH EQUATION.</p>
-          <p className="small2">IN ORDER TO GAIN THE MOST,</p>
-          <p className="small3">YOU HAVE TO KNOW HOW TO</p>
-          <p className="small4">CONVERT THE NEGATIVES</p>
-          <p className="larger">INTO POSITVES.</p>
+        <div className="quote" id="mathQuote">
+          <p className="small1 font">LIFE IS A MATH EQUATION.</p>
+          <p className="small2 font">IN ORDER TO GAIN THE MOST,</p>
+          <p className="small3 font">YOU HAVE TO KNOW HOW TO</p>
+          <p className="small4 font">CONVERT THE NEGATIVES</p>
+          <p className="larger font">INTO POSITVES.</p>
         </div>
         
       </div>
